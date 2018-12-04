6428af142be47c045f02430240b23c62b8dbd682
Allan Wang <me@allanwang.ca>
2018-12-03 21:12:40 -0500

    Auto gen new rules for numbers

--- generator/ccg.py
+++ generator/ccg.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from nltk.ccg import chart, lexicon, CCGLexicon
 from generator.formatter import format_sentence
 
@@ -21,7 +23,7 @@ from generator.formatter import format_sentence
 # and => Conj
 # ''')
 
-lex: CCGLexicon = lexicon.fromstring('''
+base_lex = '''
 :- Program, Create, Range, Int, CondPrefix, CondSuffix, CreatePart
 
 CondSuffix :: CreatePart\\CreatePart
@@ -30,7 +32,6 @@ CondPrefix :: Create/CreatePart
 create => Program/Create {\\x.program(x)}
 list => CreatePart/Range {\\x.list(x)}
 from => Range[from]/Int {\\x.x} 
-~INT~ => Int {~NUM~}
 to => (Range\\Range[from])/Int {\\y x.(MIN(x) & MAX(y))}
 to => Range/Int {\\x.x} 
 
@@ -40,19 +41,30 @@ odd => CondPrefix {\\x.odd(x)}
 odd => CondSuffix {\\x.odd(x)}
 prime => CondPrefix {\\x.prime(x)}
 prime => CondSuffix {\\x.prime(x)}
-bigger => CondSuffix {\\x.bigger(x)}
-''', True)
+bigger => CondSuffix/Int {\\y x.bigger(y, x)}
+'''
 
-parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
-words = set(lex._entries.keys())
+
+def generate_lex(integers: Set[int]) -> CCGLexicon:
+    int_rules = '\n'.join([f"{x} => Int {{{x}}}" for x in integers])
+    lex = base_lex + '\n' + int_rules
+    print(lex)
+    return lexicon.fromstring(lex, True)
+
+
+base_lexicon = lexicon.fromstring(base_lex, True)
+words = set(base_lexicon._entries.keys())
+
 
 def parse(sentence):
-    
-    formatted = format_sentence(sentence, words, keep_unknown=False)
+    formatted, numbers = format_sentence(sentence, words, keep_unknown=False)
     print(formatted)
+    lex = generate_lex(set(numbers))
+    parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
     result = parser.parse(formatted)
     for parse in result:
         chart.printCCGDerivation(parse)
         break
 
-parse("create even list from 10 to 100 that is odd and bigger")
+
+parse("create even list from 10 to 100 that is odd and bigger than 5")
