61e78724e3ed6ccc67d652b141d4cd5131c10f6e
rabldong <rabldong@gmail.com>
2018-12-04 09:06:07 +0900

    Semantic representation for creation without conditions

--- generator/ccg.py
+++ generator/ccg.py
@@ -1,21 +1,27 @@
 from nltk.ccg import chart, lexicon, CCGLexicon
-from generator.formatter import format_sentence
+from formatter import format_sentence
 
 lex: CCGLexicon = lexicon.fromstring('''
-:- Create, Range, Int
-create => Create/Range
-list => Create\\Create
-from => Range[from]/Int
-~INT~ => Int
-to => (Range\\Range[from])/Int
-to => Range/Int
-''')
+:- Create, Range, Int, Cond
+create => Create/Range {\\x.create(x)}
+list => Create\\Create {\\x.x}
+from => Range[from]/Int {\\x.x} 
+~INT~ => Int {~NUM~}
+to => (Range\\Range[from])/Int {\\y x.(MIN(x) & MAX(y))}
+to => Range/Int {\\x.x} 
+''', True)
+
 
 parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
 words = set(lex._entries.keys())
 
-
 def parse(sentence):
+    
     formatted = format_sentence(sentence, words)
-    # print(formatted)
-    return parser.parse(formatted)
+    print(formatted)
+    result = parser.parse(formatted)
+    for parse in result:
+        chart.printCCGDerivation(parse)
+        break
+
+parse("create list from 0 to 100")
