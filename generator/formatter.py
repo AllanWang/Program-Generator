e72afbbb1b660373f673383e91104c53d16dca82
Allan Wang <me@allanwang.ca>
2018-12-05 18:52:00 -0500

    Add shortcuts

--- generator/formatter.py
+++ generator/formatter.py
@@ -1,20 +1,20 @@
 from nltk import word_tokenize
-from typing import List
+from typing import List, Iterable
 
 
-def _format_word(word, words):
+def _format_word(word: str, words: Iterable[str]) -> str:
     if word in words or word.isdigit():
         return word
     return '~UNK~'
 
 
-def format_sentence(sentence, words, keep_unknown=False) -> (List[str], List[int]):
+def format_sentence(sentence: str, words: Iterable[str], keep_unknown: bool = False) -> (List[str], Iterable[int]):
     """Given sentence and collection of allowed words,
     tokenize and format the sentence"""
     tokens = word_tokenize(sentence.lower(), language='english', preserve_line=False)
     tokens = [_format_word(w, words) for w in tokens]
     numbers = [t for t in tokens if t.isdigit()]
     if keep_unknown:
-        return tokens, numbers
+        return list(tokens), numbers
     else:
-        return [w for w in tokens if w != '~UNK~'], numbers
+        return list([w for w in tokens if w != '~UNK~']), numbers
