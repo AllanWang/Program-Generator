10727a30209e7849f0543f3eecf70473deea016d
rabldong <rabldong@gmail.com>
2018-12-05 14:38:03 +0900

    code generator

--- generator/ccg.py
+++ generator/ccg.py
@@ -1,7 +1,7 @@
 from typing import Set
 
 from nltk.ccg import chart, lexicon, CCGLexicon
-from generator.formatter import format_sentence
+from formatter import format_sentence
 
 base_lex = '''
 :- Program, Create, Range, Int, CondPrefix, CondSuffix, CreatePart
@@ -12,7 +12,7 @@ CondPrefix :: Create/CreatePart
 create => Program/Create {\\x.program(x)}
 list => CreatePart/Range {\\x.list(x)}
 from => Range[from]/Int {\\x.x} 
-to => (Range\\Range[from])/Int {\\y x.(MIN(x) & MAX(y))}
+to => (Range\\Range[from])/Int {\\y x.R(x,y)}
 to => Range/Int {\\x.x} 
 
 even => CondPrefix {\\x.even(x)}
@@ -28,7 +28,7 @@ bigger => CondSuffix/Int {\\y x.bigger(y, x)}
 def generate_lex(integers: Set[int]) -> CCGLexicon:
     int_rules = '\n'.join([f"{x} => Int {{{x}}}" for x in integers])
     lex = base_lex + '\n' + int_rules
-    print(lex)
+    #print(lex)
     return lexicon.fromstring(lex, True)
 
 
@@ -38,13 +38,16 @@ words = set(base_lexicon._entries.keys())
 
 def parse(sentence):
     formatted, numbers = format_sentence(sentence, words, keep_unknown=False)
-    print(formatted)
+    #print(formatted)
     lex = generate_lex(set(numbers))
     parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
     results = parser.parse(formatted)
+    
     for result in results:
-        chart.printCCGDerivation(result)
-        break
+        return result
+        #chart.printCCGDerivation(result)
+        #return result
+        #break
 
 
-parse("create even list from 10 to 100 that is odd and bigger than 5")
+#parse("create even list from 10 to 100 that is bigger than 5")
