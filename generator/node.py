9a58cf9230ad1a56fbbc9d4f1c7b64dbbb39cde6
Allan Wang <me@allanwang.ca>
2018-12-06 18:21:51 -0500

    Fix tests for invalid nodes

--- generator/node.py
+++ generator/node.py
@@ -6,48 +6,87 @@ from typing import List, Optional
 class Node:
     value: str
     children: List['Node']
-    parent: Optional['Node']
 
     def add_child(self, value) -> 'Node':
-        child = Node(value, [], self)
+        child = Node(value, [])
         self.children.append(child)
         return child
 
     @classmethod
     def parse(cls, tree: str, down_char: chr = '(', up_char: chr = ')', sep_char: chr = ',',
-              ignore_whitespace: bool = True) -> Optional['Node']:
-        node = Node(value='', children=[], parent=None)
+              ignore_whitespace: bool = True, allow_blank_values: bool = False) -> Optional['Node']:
+        """
+        Parses the provided string into a node tree.
+        :param tree: parse input
+        :param down_char: character indicating we are going down a level
+        :param up_char: character indicating we are going up a level
+        :param sep_char: character separating nodes of the same level
+        :param ignore_whitespace: true to ignore any whitespace characters
+        :param allow_blank_values: true to allow nodes with empty string as value
+        :return: root node if parsed properly, none otherwise.
+        """
+        if tree.count(down_char) != tree.count(up_char):
+            return None
+        node: Node = Node(value='', children=[])
+        parent_stack: [Node] = []
         value: [chr] = []
 
-        def update_value():
+        def update_value(require_value: bool) -> bool:
+            """
+            Updates the value of the current node
+            :return: true if parsing should continue; false if an error occurred
+            """
             nonlocal node
             nonlocal value
+
             if value:
+                if node.value is not '':
+                    return False
                 node.value = ''.join(value)
-            value.clear()
+                value.clear()
+                return True
+            return not require_value and (allow_blank_values or node.value is not '')
 
         for c in tree:
             if c == down_char:
-                update_value()
+                if not update_value(True):
+                    return None
+                parent_stack.append(node)
                 node = node.add_child('')
             elif c == sep_char:
-                update_value()
-                node = node.parent.add_child('')
+                if not update_value(True):
+                    return None
+                if not parent_stack:
+                    return None
+                node = parent_stack[-1].add_child('')
             elif c == up_char:
-                update_value()
-                node = node.parent
+                if not update_value(False):
+                    return None
+                if not parent_stack:
+                    return None
+                else:
+                    node = parent_stack[-1]
+                    del parent_stack[-1]
             else:
                 if not ignore_whitespace or not c.isspace():
                     value.append(c)
 
-        update_value()
+        if not update_value(False):
+            return None
 
-        if node.parent is not None:
-            # bracket mismatch
+        if parent_stack:
+            # bracket mismatch; not at root
             return None
         return node
 
     def display(self, down_char: chr = '(', up_char: chr = ')', sep_char: chr = ',') -> str:
+        """
+        Returns a human readable string with the current node as root
+        :param down_char: character indicating we are going down a level
+        :param up_char: character indicating we are going up a level
+        :param sep_char: character separating nodes of the same level
+        :return:
+        """
         children_display = [c.display(down_char, up_char, sep_char) for c in self.children]
         if children_display:
             return f"{self.value}{down_char}{sep_char.join(children_display)}{up_char}"
--- generator/tests/test_node.py
+++ generator/tests/test_node.py
@@ -10,14 +10,27 @@ valid_trees = [
     "ab(cd, ef(g(c, d)))"
 ]
 
+invalid_trees = [
+    "(a)",
+    "a(b",
+    "a(b))",
+    "a(b)(c)"
+]
+
 
 class TestNode(unittest.TestCase):
 
     def test_valid_nodes(self):
         for tree in valid_trees:
             with self.subTest(tree):
-                tree = "".join(tree.split()) # Remove whitespace
+                tree = "".join(tree.split())  # Remove whitespace
                 self.assertEqual(tree, Node.parse(tree).display(), 'Parse mismatch')
+
+    def test_invalid_nodes(self):
+        for tree in invalid_trees:
+            with self.subTest(tree):
+                tree = "".join(tree.split())  # Remove whitespace
+                self.assertIsNone(Node.parse(tree), "Parsed invalid tree")
 
 
 if __name__ == '__main__':
