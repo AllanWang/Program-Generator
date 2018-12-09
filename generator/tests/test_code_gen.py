09e4d4c9847bfc89ce7f4fd55406e2431ae61606
Allan Wang <me@allanwang.ca>
2018-12-08 21:06:01 -0500

    Fix negation parsing and tested against python

--- generator/tests/test_code_gen.py
+++ generator/tests/test_code_gen.py
@@ -15,7 +15,9 @@ sentences: [(str, [int])] = [
     ("create even list from 0 to 100 that is even",
      [x for x in range(0, 101) if x % 2 == 0]),
     ("create even list from 0 to 100 that is bigger than 5",
-     [x for x in range(0, 101) if x % 2 == 0 and x > 5])
+     [x for x in range(0, 101) if x % 2 == 0 and x > 5]),
+    ("create even list from 0 to 100 that is not even",
+     [])
 ]
 
 
@@ -37,16 +39,19 @@ class TestCodeGen(unittest.TestCase):
                 if not node:
                     self.fail("Could not create node from sentence")
                 if verify_output:
-                    result = self._get_output(language.name, code)
-                    self.assertEqual(expected, result, "Output mismatch")
+                    try:
+                        result = self._get_output(language.name, code)
+                        self.assertEqual(expected, result, "Output mismatch")
+                    except:
+                        self.fail(f"Bad output\n{code}")
                 else:
                     print(f"-----\n{sentence}\n{code}\n-----")
 
     def test_python_inline(self):
-        self._test_sentence(code_gen_python_inline_templates)
+        self._test_sentence(CodeGenLanguage.from_templates('python_inline', code_gen_python_inline_templates))
 
     def test_python_functional(self):
-        self._test_sentence(code_gen_python_functional_templates)
+        self._test_sentence(CodeGenLanguage.from_templates('python_functional', code_gen_python_functional_templates))
 
     def test_kotlin(self):
         self._test_sentence(code_gen_kotlin, False)
