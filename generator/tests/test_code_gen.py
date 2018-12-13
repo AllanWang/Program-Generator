fe0bc7bb1701a6690b76c2bf99564391e76bf1ec
rabldong <rabldong@gmail.com>
2018-12-14 05:29:20 +0900

    Changed report

--- generator/tests/test_code_gen.py
+++ generator/tests/test_code_gen.py
@@ -20,7 +20,7 @@ sentences: [(str, [int])] = [
     ("create even list from 0 to 100 that is not not odd",
      []),
     ("create not even list from 0 to 100",
-     [x for x in range(0, 101) if x % 2 != 0]),
+     [x for x in range(0, 101) if x % 2 != 0])
 ]
 
 
